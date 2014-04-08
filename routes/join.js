d11232aad81f9173a88d7f062b014de421e6fb74
Davideryu Orihara <davideryu_orihara@dwango.co.jp>
2014-04-08 21:58:28 +0900

    joinの作成

--- routes/join.js
+++ routes/join.js
@@ -1,13 +1,39 @@
 var controller = require('../modules/controller');
 
 exports.list = function(req, res){
-	res.json(
-		{
-			status: 200,
-			message: 'Is Your name '+req.body.name+'?',
-			code: 200
-		}
-	);
-	controller.join();
-	res.send('Is Your name '+req.body.name+'?');
+	res.set('Content-Type', 'application/json');
+	var name = req.body.name;
+	if (!name) {
+		res.json(
+			{
+				status: 'fail',
+				errorMessage: 'no name here!'
+			}
+		);
+	} else {
+		var key = randobet(32, '');
+		res.json(
+			{
+				status: 'ok',
+				key: key,
+				message: 'Your name is '+name
+			}
+		);
+		controller.join(name, key);
+	}
+};
+
+// ランダム文字列のキーを発行する。
+var randobet = function(n, b) {
+	b = b || '';
+	var a = 'abcdefghijklmnopqrstuvwxyz' +
+		'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+		'0123456789' +
+		b;
+	a = a.split('');
+	var s = '';
+	for (var i = 0; i < n; i++) {
+		s += a[Math.floor(Math.random() * a.length)];
+	}
+	return s;
 };
